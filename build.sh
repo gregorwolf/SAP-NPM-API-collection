e0f0db4640d7ba1b9654c2dab0e6214612deb0c1
Gregor Wolf <gregor.wolf@gmail.com>
2018-11-28 22:11:42 +0100

    Copy md files to destination

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 npm search sap --registry https://npm.sap.com --json > npm.sap.com.json
 cat npm.sap.com.json | jq '.[] | .name' > packages.txt
 rpl -q '"' '' packages.txt
+
 while read package; do
   npm install $package
+done <packages.txt
+
+while read package; do
+  packageNoPrefix=`echo $package | sed 's/@sap//g'`
+  mkdir "apis$packageNoPrefix"
+  cp node_modules/$package/*.md apis$packageNoPrefix
 done <packages.txt
