de37e7106c21b672e2d90220afe2486368f06a12
Gregor Wolf <gregor.wolf@gmail.com>
2019-09-19 16:49:52 +0200

    Don't save to package.json

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 npm search sap --registry https://npm.sap.com --json > npm.sap.com.json
+cat npm.sap.com.json | jq > npm.sap.com-formatted.json
+mv npm.sap.com-formatted.json npm.sap.com.json
 cat npm.sap.com.json | jq '.[] | .name' > packages.txt
 rpl -q '"' '' packages.txt
 
 while read package; do
-  npm install $package
+  npm install $package --no-save
 done <packages.txt
 
 while read package; do
